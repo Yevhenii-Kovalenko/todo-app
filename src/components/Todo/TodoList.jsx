086ff439cdfee058eb6b49f39410995ccb264308
Yevhenii-Kovalenko <107400778+Yevhenii-Kovalenko@users.noreply.github.com>
2025-03-27 18:10:53 +0200

    feature: add animation for list item

--- src/components/Todo/TodoList.jsx
+++ src/components/Todo/TodoList.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { useQuery } from "@tanstack/react-query";
 import { fetchTodos } from "@/services/todoService";
 import { TodoItem } from "./TodoItem";
@@ -19,11 +20,24 @@ export const TodoList = () => {
       {data?.length === 0 ? (
         <p className="text-center text-2xl font-semibold">Todo list empty...</p>
       ) : (
-        <ul className="h-160 overflow-y-auto pr-2">
+        <motion.ul
+          className="h-160 overflow-y-auto pt-2 pr-2"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
           {data?.map((todo) => (
-            <TodoItem key={todo.id} todo={todo} />
+            <motion.li
+              key={todo.id}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20, transition: { duration: 0.3 } }}
+              transition={{ type: "spring", stiffness: 100 }}
+            >
+              <TodoItem key={todo.id} todo={todo} />
+            </motion.li>
           ))}
-        </ul>
+        </motion.ul>
       )}
     </>
   );
